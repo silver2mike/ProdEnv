38cd305dbf34532247496e98a10124644737be8a
MykhailoPr <86882102+silver2mike@users.noreply.github.com>
2023-01-23 22:19:58 +0200

    Update Jenkinsfile.d

--- Jenkinsfile.d
+++ Jenkinsfile.d
@@ -12,7 +12,7 @@ pipeline {
           accessKeyVariable: 'AWS_ACCESS_KEY_ID', 
           secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
           
-            sh 'terraform init'
+            sh 'terraform destroy --auto-approve'
         }
       }
     }
