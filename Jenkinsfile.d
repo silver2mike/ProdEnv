bc67f0eb270424b4d0fcf517b34d2ae5131720b1
MykhailoPr <86882102+silver2mike@users.noreply.github.com>
2023-01-23 21:38:09 +0200

    Create Jenkinsfile.d

---
+++ Jenkinsfile.d
@@ -0,0 +1,19 @@
+pipeline {
+  agent {label 'Ansible'}
+  environment {
+    ANSIBLE_PK = credentials('AWS-ProdServer-private-key')
+  }
+  stages {
+    stage('AWS Env Destroy by Terrsform') {
+      steps {
+        withCredentials([[
+          $class:            'AmazonWebServicesCredentialsBinding', 
+          credentialsId:     'AWS_Terraform', 
+          accessKeyVariable: 'AWS_ACCESS_KEY_ID', 
+          secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+          
+            sh 'terraform destroy --auto-approve'
+       }
+     }
+   }
+ }
