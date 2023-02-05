33556e8e54df8914f31d5034acbffca1eba474e0
MPryshchepnyi <silver2mike@gmail.com>
2023-02-05 21:42:11 +0200

    main test

--- main.tf
+++ main.tf
@@ -61,13 +61,22 @@ resource "aws_security_group" "Prod_Env" {
   name = "Prod SG"
 }
 
-resource "aws_security_group_rule" "Inbound" {
+resource "aws_security_group_rule" "HTTP" {
     type = "ingress"
     description = "HTTP"
     from_port = 80
     to_port = 80
     protocol = "tcp"
     source_security_group_id = aws_security_group.LB.id
+    security_group_id = aws_security_group.Prod_Env.id
+}
+resource "aws_security_group_rule" "SSH" {
+    type = "ingress"
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_block = ["0.0.0.0/0"]
     security_group_id = aws_security_group.Prod_Env.id
 }
 resource "aws_security_group_rule" "Outbound" {
