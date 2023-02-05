68fd2d90e1797a2ef5db9a6b95318388e9c6b131
MPryshchepnyi <silver2mike@gmail.com>
2023-02-05 22:25:03 +0200

    main test

--- user_data.sh
+++ user_data.sh
@@ -1,22 +1,3 @@
-Content-Type: multipart/mixed; boundary="//"
-MIME-Version: 1.0
-
---//
-Content-Type: text/cloud-config; charset="us-ascii"
-MIME-Version: 1.0
-Content-Transfer-Encoding: 7bit
-Content-Disposition: attachment; filename="cloud-config.txt"
-
-#cloud-config
-cloud_final_modules:
-- [scripts-user, always]
-
---//
-Content-Type: text/x-shellscript; charset="us-ascii"
-MIME-Version: 1.0
-Content-Transfer-Encoding: 7bit
-Content-Disposition: attachment; filename="userdata.txt"
-
 #!/bin/bash
 sudo apt -y update
 sudo apt install git -y
@@ -24,5 +5,3 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt install ansible -y
 sudo git clone https://github.com/silver2mike/ProdEnv.git
 ansible-playbook ProdEnv/prod.yml
-
---//--
