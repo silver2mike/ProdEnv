be93ad5bbc15bb0fdc6e7c4bf0b491aee53c0c01
MPryshchepnyi <silver2mike@gmail.com>
2023-02-06 12:49:06 +0200

    LB changed

--- main.tf
+++ main.tf
@@ -162,21 +162,20 @@ resource "aws_lb" "Prod_env_LB" {
 //    availability_zones = [data.aws_availability_zones.az.names[0], data.aws_availability_zones.az.names[1]]
     security_groups = [aws_security_group.LB.id]
     subnets = [data.aws_subnet_ids.def_sub.ids[0]
-/*
-    listener {
-        lb_port             = 80
-        lb_protocol         = "http"
-        instance_port       = 80
-        instance_protocol   = "http"
-    }
-    health_check {
-        healthy_threshold   = 2
-        unhealthy_threshold = 2
-        timeout             = 3
-        target              = "HTTP:80/"
-        interval            = 10
-    }
-*/
+#    listener {
+#        lb_port             = 80
+#        lb_protocol         = "http"
+#        instance_port       = 80
+#        instance_protocol   = "http"
+#    }
+#    health_check {
+#        healthy_threshold   = 2
+#        unhealthy_threshold = 2
+#        timeout             = 3
+#        target              = "HTTP:80/"
+#        interval            = 10
+#    }
+#
     tags = {
         Name = "Prod Environment"
     }
