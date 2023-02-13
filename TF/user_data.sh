4fa196a42f79c8a7da5acd06b2395e4c6a0a11ab
MPryshchepnyi <silver2mike@gmail.com>
2023-02-13 14:46:04 +0200

    13022023

--- TF/user_data.sh
+++ TF/user_data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo apt -y update
-sudo apt install git -y
+#sudo apt install git -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt install ansible -y
-sudo git clone https://github.com/silver2mike/ProdEnv.git
-ansible-playbook ProdEnv/prod.yml
+#sudo git clone https://github.com/silver2mike/ProdEnv.git
+wget https://raw.githubusercontent.com/silver2mike/ProdEnv/main/prod.yml
+ansible-playbook prod.yml
